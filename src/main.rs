33d82fcdca8f561cb9d4e67eecd1ee905cd40065
disrupted <hi@salomonpopp.me>
2021-04-28 01:57:09 +0200

    Post request to blackd

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,38 @@ use std::{io, io::prelude::*};
 pub type Error = Box<dyn std::error::Error + Send + Sync>;
 pub type Result<T> = std::result::Result<T, Error>;
 
-pub fn main() -> Result<()> {
-    for line in io::stdin().lock().lines() {
-        println!("{}", line?);
+fn main() -> Result<()> {
+    let _result = read_stdin();
+    let _result = format();
+    Ok(())
+}
+
+fn read_stdin() -> Result<()> {
+    let stdin = io::stdin();
+    let lines = stdin.lock().lines();
+    // call blackd
+
+    // TODO
+    if true == true {
+        for line in lines {
+            println!("{}", line?);
+        }
     }
+    Ok(())
+}
+
+fn format() -> Result<()> {
+    let client = reqwest::blocking::Client::new();
+    let mut resp = client
+        .post("http://localhost:45484")
+        .body("print('bli bla blub')")
+        .send()?;
+
+    // println!("res = {:?}", resp);
+
+    let mut body = String::new();
+    resp.read_to_string(&mut body)?;
+    println!("{}", body);
+
     Ok(())
 }
