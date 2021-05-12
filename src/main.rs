b69235b4cbba15dea674d35e87c1ca5d7678b5a4
Salomon Popp <hi@salomonpopp.me>
2021-05-12 23:16:16 +0200

    Add tests & use match statement (#15)

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use std::{io, io::prelude::*};
 
-pub type Error = Box<dyn std::error::Error + Send + Sync>;
-pub type Result<T> = std::result::Result<T, Error>;
+const BLACKD_DEFAULT_URL: &str = "http://localhost:45484";
 
 fn main() {
     let stdin = read_stdin();
-    let _result = format(stdin.unwrap());
+    let result = format(BLACKD_DEFAULT_URL, stdin.unwrap());
+    match result {
+        Ok(v) => print!("{}", v),
+        Err(e) => print!("Error formatting with blackd-client: {}", e),
+    }
 }
 
-fn read_stdin() -> Result<String> {
+fn read_stdin() -> Result<String, Box<dyn std::error::Error + Send + Sync>> {
     let mut buffer = String::new();
     let stdin = io::stdin();
     {
@@ -18,22 +21,71 @@ fn read_stdin() -> Result<String> {
     Ok(buffer)
 }
 
-fn format(stdin: String) -> Result<()> {
-    let resp = minreq::post("http://localhost:45484")
+fn format(url: &str, stdin: String) -> Result<String, minreq::Error> {
+    let resp = minreq::post(url)
         .with_header("X-Fast-Or-Safe", "fast")
         .with_body(stdin.as_str())
         .send()?;
 
-    // input is already well-formatted
-    if resp.status_code == 204 {
-        print!("{}", stdin);
-        return Ok(());
+    match resp.status_code {
+        204 => Ok(stdin),                      // input is already well-formatted
+        200 => Ok(resp.as_str()?.to_string()), // input was reformatted by Black
+        _ => Err(minreq::Error::Other("Error")),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use httpmock::MockServer;
+
+    #[test]
+    fn test_format_success() {
+        let server = MockServer::start();
+        let body = "print(\"Hello World!\")";
+        let mock = server.mock(|when, then| {
+            when.method("POST")
+                .path("/")
+                .header("X-Fast-Or-Safe", "fast");
+            then.status(200).body(body);
+        });
+
+        let result = format(server.url("").as_str(), "print('Hello World!')".to_string());
+
+        mock.assert();
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), body);
     }
 
-    // input was reformatted by Black
-    if resp.status_code == 200 {
-        print!("{}", resp.as_str()?);
+    #[test]
+    fn test_format_unchanged() {
+        let server = MockServer::start();
+        let body = "print(\"Already formatted\")";
+        let mock = server.mock(|when, then| {
+            when.method("POST")
+                .path("/")
+                .header("X-Fast-Or-Safe", "fast");
+            then.status(204);
+        });
+
+        let result = format(server.url("").as_str(), body.to_string());
+
+        mock.assert();
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), body);
     }
 
-    Ok(())
+    #[test]
+    fn test_format_error() {
+        let server = MockServer::start();
+        let mock = server.mock(|when, then| {
+            when.method("POST");
+            then.status(418);
+        });
+
+        let result = format(server.url("").as_str(), String::new());
+
+        mock.assert();
+        assert!(result.is_err());
+    }
 }
